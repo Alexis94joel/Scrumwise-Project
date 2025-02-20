02e90e58c9e45536cd0180af759c0d1404524ee3
Alexis94joel <99000336+Alexis94joel@users.noreply.github.com>
2025-02-20 12:18:42 -0600

    Delete Simulating the Dice Roll.py

--- Simulating the Dice Roll.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def roll_dice():
-    die1 = random.randint(1, 6)
-    die2 = random.randint(1, 6)
-    return die1, die2
-
-dice = roll_dice()
-print(f"You rolled: {dice[0]} and {dice[1]} (Total : {sum(dice)})")
