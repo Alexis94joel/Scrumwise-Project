2b55bc9f980d5be2d993be818780a84035975002
Alexis94joel <99000336+Alexis94joel@users.noreply.github.com>
2025-02-22 00:18:20 -0600

    Update DiceRoll.java

--- DiceRoll.java
+++ DiceRoll.java
@@ -7,4 +7,9 @@ public class DiceRoll {
         int die2 = rand.nextInt(6) + 1;
         return new int[]{die1, die2};
     }
+
+    public static void main(String[] args) {
+                int[] dice = rollDice();
+                System.out.println("You rolled: " + dice[0] + " and " + dice[1] + " (Total: " + (dice[0] + dice[1]) + ")");
+    }
 }
